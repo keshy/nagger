743c8000290f6ee0d3a24f1627705cb78b0fb606
Krishnan Narayan <schoolhouse@Krishnans-Mac-mini.local>
2022-08-09 17:05:43 -0700

    Adding email dispatch

--- reminder_lambda.py
+++ reminder_lambda.py
@@ -1,8 +1,16 @@
 import base64
 import json
+import os
 from datetime import datetime
+from sendgrid import SendGridAPIClient, SendGridException
+from sendgrid.helpers.mail import Mail
 
 THRESHOLD_NOTIFICATION_DAYS = 30
+
+EMAIL_NOTIFICATION_TEMPLATE = """
+    
+
+"""
 
 reminder_config = {
     "auto_policy": {
@@ -88,7 +96,36 @@ def process_checks():
             continue
     # prepar notification/dispatch
     # dispatch_notification(violation_keys)
-    print("Found %s violations in total" % (len(violation_keys)))
+    process_notification(violation_keys)
+
+
+def process_notification(violations=None):
+    to_mails = os.getenv('TO_EMAILS', None)
+    API_KEY = os.getenv('SG_API_KEY', None)
+    if to_mails is None or API_KEY is None:
+        print(
+            "Error dispatching notification. Required env vars are not populated. Current values are TO_EMAILs=%s and SG_API_KEY=%s" % (
+                to_mails, API_KEY))
+        exit(1)
+    if violations is None:
+        violations = []
+    html_content = ''
+    if len(violations) == 0:
+        html_content = 'No upcoming violations for this week'
+    else:
+        content = [reminder_config.get(v) for v in violations]
+        raw_content = "\n \n Please find the list violations below: \n \n"
+        html_content = raw_content + json.dumps(content, indent=4, sort_keys=True)
+    to_mails = to_mails.split(',')
+    message = Mail(from_email='keshi8086@gmail.com', to_emails=to_mails,
+                   subject='Nagger Reminder: %s violations found' % (len(violations)),
+                   plain_text_content=html_content)
+    try:
+        sg = SendGridAPIClient(API_KEY)
+        response = sg.send(message)
+        print("Status code for email dispatch: %s" % (response.status_code))
+    except SendGridException as e:
+        print(e)
 
 
 if __name__ == "__main__":
